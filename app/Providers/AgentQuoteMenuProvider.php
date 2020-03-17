ba03c4ec5e0d6908dc586618d0c0c6e1bb4ec74a
Franklin Decker III <frank@dzineer.com>
2020-03-18 03:16:27 +0700

    Backend menu debugging... ongoing....

--- app/Providers/AgentQuoteMenuProvider.php
+++ app/Providers/AgentQuoteMenuProvider.php
@@ -127,7 +127,8 @@ class AgentQuoteMenuProvider extends ServiceProvider
                                     ], true));
 
                                     if (count($menuItemsToAdd)) {
-                                        $event->menu->add(...$menuItemsToAdd);
+                                        foreach($menuItemsToAdd as $menuItem)
+                                        $event->menu->add($menuItem);
                                     }
 
 
