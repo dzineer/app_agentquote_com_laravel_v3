64442753e3cff363daa464cefc35ee3894d6e82b
Franklin Decker III <frank@dzineer.com>
2020-03-23 17:38:00 +0700

    Debugging Landing Page

--- resources/views/landing-pages/pages/topbar.blade.php
+++ resources/views/landing-pages/pages/topbar.blade.php
@@ -1,6 +1,6 @@
         <top-bar :icons="{{ $social_media }}" phone="{{ $user->profile->contact_phone }}"></top-bar>
         <responsive-menu :menu="items"></responsive-menu>
-        @if($branding['user_log'])
+        @if($branding['user_logo'])
         <header-logo path="{!! $user->profile->logo !!}" target="self" link="/"></header-logo>
         @else
             {{ $branding['company']['name'] }}
