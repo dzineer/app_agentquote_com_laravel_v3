6afc65a44f972acd92c8238aa490db61c525fe44
Franklin Decker III <frank@dzineer.com>
2020-03-27 22:26:57 +0700

    Adding Branding Bar

--- resources/views/landing-pages/pages/topbar.blade.php
+++ resources/views/landing-pages/pages/topbar.blade.php
@@ -12,7 +12,7 @@
             </div>
             <div slot="right">
                 <div class="tw-flex tw-flex-col">
-                    <p class="tw-text-primary tw-text-sm tw-uppercase tw-tracking-widest tw-text-xs">by appointment/mon-sat</p>
+                    <p class="tw-text-primary tw-uppercase tw-tracking-widest tw-text-xs">by appointment/mon-sat</p>
                     <a class="tw-bg-primary hover:tw-bg-blue-700 tw-text-white tw-rounded tw-text-center tw-py-4 tw-px-6 focus:tw-outline-none focus:tw-shadow-outline tw-uppercase tw-text-base tw-font-bold">
                         book an appointment
                     </a>
