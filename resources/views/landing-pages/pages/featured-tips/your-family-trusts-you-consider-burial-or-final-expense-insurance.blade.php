230629bf51f7a75c3fc69514fddacb91ac235a5b
Franklin Decker III <frank@dzineer.com>
2020-04-04 02:58:56 +0700

    Added  featured tips

--- resources/views/landing-pages/pages/featured-tips/your-family-trusts-you-consider-burial-or-final-expense-insurance.blade.php
+++ resources/views/landing-pages/pages/featured-tips/your-family-trusts-you-consider-burial-or-final-expense-insurance.blade.php
@@ -90,7 +90,7 @@
 
                             </li>
                             <li class="tw-text-sm tw-tracking-tight tw-w-full tw-font-light tw-my-1 tw-text-left">
-                                <a class="tw-text-primary tw-text-xl md:tw-text-md" href="/featured-tips/but-i-already-have-insurance#featured-tips" target="_self">What happens to my policy if I change my mortgage?</a>
+                                <a class="tw-text-primary tw-text-xl md:tw-text-md" href="/featured-tips/what-happens-to-my-policy-if-i-change-my-mortgage#featured-tips" target="_self">What happens to my policy if I change my mortgage?</a>
                             </li>
                         </ul>
                     </div>
