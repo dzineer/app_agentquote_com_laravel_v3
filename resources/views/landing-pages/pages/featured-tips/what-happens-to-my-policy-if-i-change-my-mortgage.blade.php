6034eb8db9a131fb8bb12df62cb4d8b1db0031d1
Franklin Decker III <frank@dzineer.com>
2020-04-04 03:18:01 +0700

    Added  featured tips

--- resources/views/landing-pages/pages/featured-tips/what-happens-to-my-policy-if-i-change-my-mortgage.blade.php
+++ resources/views/landing-pages/pages/featured-tips/what-happens-to-my-policy-if-i-change-my-mortgage.blade.php
@@ -43,26 +43,18 @@
                             If you are accessing equity from your home, you will need to make sure that your policy meets the new value of your mortgage.
                         </p>
 
-                        <ol class="tw-my-6 tw-leading-normal">
-                            <li>
-                                <p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
-                                    You could get a new mortgage protection policy for the total amount of your new mortgage, or just for the home equity amount.
-                                </p>
+                        <ol class="tw-my-6 tw-leading-normal tw-list-disc tw-list-inside">
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
+                                You could get a new mortgage protection policy for the total amount of your new mortgage, or just for the home equity amount.
                             </li>
-                            <li>
-                                <p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
-                                    Compare the costs and benefits of both options. It may be cheaper to keep your original mortgage protection policy and then buy a second policy for the equity amount. Check the cost of cancelling the original policy and replacing it with a policy for the full amount of your new mortgage.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
+                                Compare the costs and benefits of both options. It may be cheaper to keep your original mortgage protection policy and then buy a second policy for the equity amount. Check the cost of cancelling the original policy and replacing it with a policy for the full amount of your new mortgage.
                             </li>
-                            <li>
-                                <p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
-                                    Whether you are accessing home equity or extending the term and need to get a new policy, you may find that your premium is higher than the last time you took out coverage. This is because you are older, and your age affects your premium and the benefit may be higher or the term longer.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
+                                Whether you are accessing home equity or extending the term and need to get a new policy, you may find that your premium is higher than the last time you took out coverage. This is because you are older, and your age affects your premium and the benefit may be higher or the term longer.
                             </li>
-                            <li>
-                                <p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
-                                    Be cautious of your health. If your health hasn’t changed for the better, you may no longer qualify for a new policy. However, if you have given up smoking, or if rates have come down since the last time you applied for coverage, you may be able to get cheaper coverage.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
+                                Be cautious of your health. If your health hasn’t changed for the better, you may no longer qualify for a new policy. However, if you have given up smoking, or if rates have come down since the last time you applied for coverage, you may be able to get cheaper coverage.
                             </li>
                         </ol>
 
@@ -78,17 +70,14 @@
                             <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left">
                                If you have your own policy, then nothing really needs to be done. You and your family simply discuss the goals of your policy and the new lender. It is a good time to review your coverage to see if the policy is still meeting your goals.
                             </li>
-                            <li><p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left">
-                                    If you have a policy through your lender’s group arrangement, your lender will cancel the policy when you switch your mortgage lender.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left">
+                               If you have a policy through your lender’s group arrangement, your lender will cancel the policy when you switch your mortgage lender.
                             </li>
-                            <li><p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left">
-                                    If you must get a new policy it may cost you more, as you will be older than when you first took out the policy. If you are not in good health, you will have to pay a higher premium, or you may not be able to get coverage at all to pay off your mortgage.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left">
+                               If you must get a new policy it may cost you more, as you will be older than when you first took out the policy. If you are not in good health, you will have to pay a higher premium, or you may not be able to get coverage at all to pay off your mortgage.
                             </li>
-                            <li><p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left">
-                                    You may, however, be able to qualify for a policy to pay down your mortgage or protect your monthly payments for a given period of time. Protecting your mortgage payments will give your family time to figure out their next steps without the worry of how they’ll pay the monthly mortgage premium.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left">
+                               You may, however, be able to qualify for a policy to pay down your mortgage or protect your monthly payments for a given period of time. Protecting your mortgage payments will give your family time to figure out their next steps without the worry of how they’ll pay the monthly mortgage premium.
                             </li>
                         </ol>
 
@@ -110,20 +99,14 @@
                         </p>
 
                         <ol class="tw-my-6 tw-leading-normal tw-list-disc tw-list-inside">
-                            <li>
-                                <p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
-                                    If you decide to keep paying for the policy after paying off the mortgage, you will still be covered by the policy in full up until it expires.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
+                                If you decide to keep paying for the policy after paying off the mortgage, you will still be covered by the policy in full up until it expires.
                             </li>
-                            <li>
-                                <p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
-                                    As most mortgage protection policies are funded through a term life insurance policy, it could be convertible. Meaning if the policy is “convertible” then you could decide to convert some or all of the benefit to a permanent life insurance policy. The cost for the new converted policy will be higher than the original policy, however, there are no health qualifications for this change. A great feature if your health has changed preventing you from getting a new life insurance policy.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
+                                As most mortgage protection policies are funded through a term life insurance policy, it could be convertible. Meaning if the policy is “convertible” then you could decide to convert some or all of the benefit to a permanent life insurance policy. The cost for the new converted policy will be higher than the original policy, however, there are no health qualifications for this change. A great feature if your health has changed preventing you from getting a new life insurance policy.
                             </li>
-                            <li>
-                                <p class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
-                                    If you die before the policy expires, the benefit would no longer need to be used to pay off or pay down your mortgage. So, any benefit would be paid to your beneficiaries tax-free.
-                                </p>
+                            <li class="tw-text-base tw-tracking-tight tw-w-full tw-font-light tw-text-left tw-my-4">
+                                If you die before the policy expires, the benefit would no longer need to be used to pay off or pay down your mortgage. So, any benefit would be paid to your beneficiaries tax-free.
                             </li>
                         </ol>
 
