51fc341f4676dae4fc210124a2e5aca6c772bbca
Franklin Decker III <frank@dzineer.com>
2020-04-07 18:59:08 +0700

    troubleshooting refactored quoters

--- app/Quoters/AquoterGateway.php
+++ app/Quoters/AquoterGateway.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Libraries;
+namespace App\Quoters;
 
 class AquoterGateway {
 
@@ -35,8 +35,8 @@ class AquoterGateway {
             $get_data 	=  $data;
             $get_data['access_token'] = $this->access_token;
 
-       /* 
-        $get_data 	= 	array(	
+       /*
+        $get_data 	= 	array(
             'access_token' =>   $this->access_token,
             'id'           =>   $data['id'],
             'state'        => 	$data['state'],
@@ -50,9 +50,9 @@ class AquoterGateway {
             'period'       => 	$data['period'] );
         */
 
-        $this->setHostname( 'http://149.28.45.153' );    
-        $this->setEndpoint( '/v1/quotes?' );   
-        
+        $this->setHostname( 'http://149.28.45.153' );
+        $this->setEndpoint( '/v1/quotes?' );
+
         // echo "<br>get_data: " . print_r($get_data,true) . '<br>';
 
         // echo "<br>" . 'https://api.aq2e.com/v1/quotes?access_token=7c25c7552fb3015a64df141b87d4c3d9047b81ce&id=3&term=10&state=CA&period=0&month=9&day=4&year=1974&gender=m&tobacco=n&benefit=100';
@@ -62,19 +62,19 @@ class AquoterGateway {
     }
 
 	function getAccessToken( $account ) {
-        
-        $post_data 		= 	array(	
+
+        $post_data 		= 	array(
             'client_id'     => 	$account->client_id,
             'client_secret' => 	$account->client_secret,
             'grant_type'    => 	$account->grant_type,
         );
 
         /*
-        $post_data 		= 	array(	
+        $post_data 		= 	array(
             'client_id'     => 	'fdecker',
             'client_secret' => 	'fdecker',
             'grant_type'    => 	'client_credentials',
-            
+
         );
         */
 
@@ -93,7 +93,7 @@ class AquoterGateway {
         $urlRequest = $this->hostname . $this->endpoint . $get_string;
 
 //        echo "<br>URL: " . $urlRequest . '<br>';
-        
+
         $curl_connection = 	curl_init($urlRequest);
 
         curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
@@ -110,8 +110,8 @@ class AquoterGateway {
 
         curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 1);
 
-        curl_setopt($curl_connection, CURLOPT_URL, $urlRequest);		
-        
+        curl_setopt($curl_connection, CURLOPT_URL, $urlRequest);
+
         $result = curl_exec($curl_connection);// perform our request
 
         curl_close($curl_connection);// close the connection
@@ -143,9 +143,9 @@ class AquoterGateway {
         curl_setopt($curl_connection, CURLOPT_POSTFIELDS, $post_string);// set data to be posted
 
         $result = curl_exec($curl_connection);// perform our request
-        
+
 //      echo "<br>result: " . print_r($result,true) . '<br>';
-        
+
         curl_close($curl_connection);// close the connection
 
         return $result;
