598ccb3960194a6516db1a55940e9b3925bfdc23
Franklin Decker III <frank@dzineer.com>
2020-04-08 22:00:27 +0700

    updated responsiveness

--- resources/views/landing-pages/pages/topbar.blade.php
+++ resources/views/landing-pages/pages/topbar.blade.php
@@ -13,7 +13,7 @@
             <div slot="right">
                 @if($book_appointment->hasLink)
                 <div class="tw-flex tw-flex-col max-xs:tw-mt-6 max-xs:tw-mb-2">
-                    <p class="tw-text-primary tw-uppercase tw-tracking-widest tw-text-sm">by appointment/mon-sat</p>
+                    {{--<p class="tw-text-primary tw-uppercase tw-tracking-widest tw-text-sm">by appointment/mon-sat</p>--}}
                     <a href="{{ $book_appointment->link }}" target="_blank" class="tw-bg-primary hover:tw-bg-blue-700 tw-text-white tw-rounded tw-text-center tw-py-3 tw-px-3 focus:tw-outline-none focus:tw-shadow-outline tw-uppercase tw-text-sm tw-font-bold">
                         book an appointment
                     </a>
