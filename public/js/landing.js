3408de6e794afaa845beec49b9647a28e1b7edfa
Franklin Decker III <frank@dzineer.com>
2020-04-08 22:41:36 +0700

    updated responsiveness

--- public/js/landing.js
+++ public/js/landing.js
@@ -40368,7 +40368,7 @@ var render = function() {
             "div",
             {
               staticClass:
-                "tw-flex tw-w-full tw-justify-between tw-items-center max-xs:tw-flex-col"
+                "tw-flex tw-w-full tw-justify-between tw-items-center max-xs:tw-flex-col-disable"
             },
             [_vm._t("left"), _vm._v(" "), _vm._t("right")],
             2
--- resources/assets/js/vuejs/components/BrandBar.vue
+++ resources/assets/js/vuejs/components/BrandBar.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="tw-w-full tw-flex tw-justify-center tw-items-center tw-mt-12">
     <div class="dz:section tw-py-4 tw-border-b tw-flex tw-flex-row tw-items-center tw-justify-start tw-text-white tw-w-full" style="min-height: 45px;">
-        <div class="tw-flex tw-w-full tw-justify-between tw-items-center max-xs:tw-flex-col">
+        <div class="tw-flex tw-w-full tw-justify-between tw-items-center max-xs:tw-flex-col-disable">
 
             <slot name="left"></slot>
 
