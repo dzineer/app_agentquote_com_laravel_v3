6cc1298a6bd2e6bae3ce17eec271362a69316beb
Franklin Decker III <frank@dzineer.com>
2020-04-09 21:56:47 +0700

    updated carrier details

--- app/Http/Controllers/ModulesLandingPage.php
+++ app/Http/Controllers/ModulesLandingPage.php
@@ -1308,9 +1308,8 @@ class ModulesLandingPage extends Controller
                         "policy" => $quote['ProductName'],
                         "reference" => $quote['Reference'],
                         "links" => [
-                            ['text' => 'Company Information', 'href' => '', 'icon' => 'file-text-o' ],
-                        //    ['text' => 'Click Here to Match a rate to your Health Profile', 'href' => $quote['link1'], 'icon' => 'external-link' ],
-                        //    ['text' => 'View Policy Details', 'href' => $quote['link2'], 'icon' => 'file-text-o' ],
+                            ['text' => 'Click Here to Match a rate to your Health Profile', 'href' => $quote['link1'], 'icon' => 'external-link' ],
+                            ['text' => 'View Policy Details', 'href' => $quote['link2'], 'icon' => 'file-text-o' ],
                         ],
                         'logo' => env('AWS_S3_CDN') . '/logos/' . $quote['BannerLogoImageURL'],
                         'rateClassifications' => [
