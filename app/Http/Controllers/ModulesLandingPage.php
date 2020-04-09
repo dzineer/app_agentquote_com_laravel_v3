85e289c71b1e8e7227a0d3adcbb5b8ab8b554e15
Franklin Decker III <frank@dzineer.com>
2020-04-09 23:56:11 +0700

    fixing quoter

--- app/Http/Controllers/ModulesLandingPage.php
+++ app/Http/Controllers/ModulesLandingPage.php
@@ -612,7 +612,7 @@ class ModulesLandingPage extends Controller
 
         $quoteRequest = \unserialize( $verifiedQuote['data'] );
 
-        // dd($verifiedQuote);
+        dd($verifiedQuote);
 
         $user = User::find($quoteRequest['user_id']);
 
