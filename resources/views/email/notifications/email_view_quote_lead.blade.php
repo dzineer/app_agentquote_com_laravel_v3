a2b543bd2fa016e8c99b0e218ac8247a13cfbd96
Franklin Decker III <frank@dzineer.com>
2020-04-15 01:06:29 +0700

    Emailing quote lead after verification

--- resources/views/email/notifications/email_view_quote_lead.blade.php
+++ resources/views/email/notifications/email_view_quote_lead.blade.php
@@ -26,7 +26,7 @@
                 <?php
                 $formatter = new \NumberFormatter('en_US',  \NumberFormatter::CURRENCY);
                 $symbol = $formatter->getSymbol(\NumberFormatter::INTL_CURRENCY_SYMBOL);
-                $benefit = $formatter->formatCurrency(intval($quote->benefit), $symbol)
+                $benefit = $formatter->formatCurrency(intval($quote["benefit"]), $symbol)
                 ?>
                 <p style="margin-bottom:12px">
                     You have just received the following quote lead::<br><br>
