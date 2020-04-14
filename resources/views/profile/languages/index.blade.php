df1eb7d168a869881d789be6590b1cb31d344f0b
Franklin Decker III <frank@dzineer.com>
2020-04-14 20:58:22 +0700

    Testing user profile language settings

--- resources/views/profile/languages/index.blade.php
+++ resources/views/profile/languages/index.blade.php
@@ -47,7 +47,6 @@
 
                                         <label class="form-check-label">
                                             <input class="form-check-input languages" type="checkbox" id="language_{{$language->language_id}}" name="language_{{$language->language_id}}" value="{{$language->language_id}}" {{ $language->selected == 1 ? 'checked="checked"' : '' }} data-languages="language_{{$language->language_id}}">
-                                            <input type="hidden" id="hidden_language__{{$language->language_id}}"  name="hidden_language__{{$language->language_id}}" value="{{ $language->selected == 1 ? $language->language_id : '' }}">
                                             @if($language->subtag)
                                                 {{ $language->name . ' ' . '(' . strtoupper($language->subtag) . ')' }}
                                             @else
@@ -89,94 +88,6 @@
             font-weight: bold !important;
         }
     </style>
-
-    <script>
-
-        jQuery(function() {
-
-        });
-
-        (function( options ) {
-            var $ = {
-                libs: typeof options.libs !== 'undefined' ? options.libs : {},
-                checkAll: typeof options.checkAll !== 'undefined' ? options.checkAll : '',
-                checkboxes: typeof options.checkboxes !== 'undefined' ? options.checkboxes : '',
-                labels: typeof options.labels !== 'undefined' ? options.labels : '',
-                checkAllLabel: typeof options.checkAllLabel !== 'undefined' ? options.checkAllLabel : '',
-                checkAllInst: '',
-                checkBoxesInst: '',
-                labelInst: '',
-                allChecked: true,
-                fn: (function() {
-                    return {
-                        init: function() {
-                            $.checkAllInst = $.libs.jq( $.checkAll );
-                            $.labelInst = $.libs.jq( $.checkAllLabel );
-                            $.checkBoxesInst = $.libs.jq( $.checkboxes );
-
-                            $.libs.jq(document).on('click', options.checkAll, $.fn.onClick);
-
-                            $.checkBoxesInst.each( function( index, obj) {
-                                $.libs.jq(this).on("click", function() {
-                                    var val = $.libs.jq(this).val();
-                                    var hidden_field = $.libs.jq("#hidden_language__" + val);
-                                    console.log(val);
-                                    if ($.libs.jq(this)[0].checked) {
-                                        hidden_field.val(0);
-                                        $.libs.jq(this).attr('checked', true);
-                                    } else {
-                                        hidden_field.val(val);
-                                        $.libs.jq(this).attr('checked', false);
-                                    }
-                                });
-
-                                if (! this.checked ) { // if any of the checkboxes are not checked then we know that checkall should not be checked yet
-                                    console.log(obj);
-                                    $.allChecked = false;
-                                }
-                            });
-
-                            if ( $.allChecked ) {
-                                $.checkAllInst.attr("checked", "checked");
-                                $.labelInst.html( $.labels.on );
-                            }
-                            else {
-                                $.labelInst.html( $.labels.off );
-                            }
-                        },
-                        onClick: function( e ) {
-                            if (!this.checked) { // all is selected so if clicked deselect all
-                                $.checkBoxesInst.each( function( index, obj ) {
-                                    this.removeAttribute("checked");
-                                    $.labelInst.html( $.labels.on );
-                                });
-                            }
-                            else {
-                                $.checkBoxesInst.each( function( index, obj ) {
-                                    this.setAttribute("checked", "checked");
-                                    $.labelInst.html( $.labels.off );
-                                });
-                            }
-                        }
-                    }
-                }())
-            };
-
-            $.fn.init();
-
-        }(
-            {
-                'libs': { jq: jQuery },
-                'checkAll': '#language-check-all',
-                'checkboxes': '.languages:checkbox',
-                'checkAllLabel': '#select-all-label',
-                'labels': {
-                    'on' : 'Deselect All Languages',
-                    'off': 'Select All Languages'
-                }
-            }
-        ))
-    </script>
 
     @if (isset($message))
         <script>
