1bb0b17c4c76585d0f1b2ee61d0bd7d015883cb9
Franklin Decker III <frank@dzineer.com>
2020-04-14 21:47:57 +0700

    Testing user profile language settings and added languages to contact us banner

--- resources/assets/js/vuejs/components/ContactUsBanner.vue
+++ resources/assets/js/vuejs/components/ContactUsBanner.vue
@@ -2,7 +2,7 @@
     <div class="dz:section tw-py-8 tw-w-full tw-flex tw-flex-col tw-justify-center tw-items-center tw-bg-primary tw-text-center tw-text-white" style="min-height: 260px;">
         <h3 class="tw-text-3xl sm:tw-text-4xl tw-my-6">Need to talk to us directly?</h3>
         <p class="tw-text-xl sm:tw-text-2xl tw-mb-6">Call us with questions or to apply by phone. You can also call with questions or to apply by phone in
-            <span v-for="language in languages" v-text="language" class="tw-mr-1"></span>
+            <languages-spoken :language="language"></languages-spoken>
         .</p>
         <div class="tw-text-4xl sm:tw-text-5xl tw-mb-6">
             <display-phone-number :phone="phone"></display-phone-number>
@@ -14,8 +14,12 @@
 </template>
 
 <script>
+    import LanguagesSpoken from "./LanguagesSpoken";
     export default {
-        props: ['phone','offeredby', 'languages']
+        props: ['phone','offeredby', 'languages'],
+        components: {
+            LanguagesSpoken
+        }
     }
 
 </script>
---
+++ resources/assets/js/vuejs/components/LanguagesSpoken.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>
+        <span v-for="language in languages" v-text="language" class="tw-mr-1"></span>
+    </p>
+</template>
+
+<script>
+    export default {
+        props: ['languages']
+    }
+</script>
