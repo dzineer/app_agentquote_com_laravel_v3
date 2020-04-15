45462e3d1cf522fa064d98106bcadd13abec3430
Franklin Decker III <frank@dzineer.com>
2020-04-16 02:11:39 +0700

    Working on LetsEncryptCommand

--- vendor/dzineer/reseller-club-v2/src/Dns/Txt/TxtRecord.php
+++ vendor/dzineer/reseller-club-v2/src/Dns/Txt/TxtRecord.php
@@ -106,7 +106,7 @@ class TxtRecord
         }
 
         $response = $this->api->get(
-            'dns/manage/search-records.xml',
+            'dns/manage/search-records.json',
             $data
         );
 
@@ -124,7 +124,7 @@ class TxtRecord
         );
 
         // httpapi.com/api/dns/manage/search-records.json?auth-userid=0&api-key=key&domain-name=domain.asia&type=A&no-of-records=10&page-no=1
-        $url = 'https://httpapi.com/api/dns/manage/search-records.json?' .
+        $url = 'https://httpapi.com/api/dns/manage/search-records.xml?' .
             'auth-userid=784909' .
             '&api-key=GPFOx3p9Y7byCpiZwaP3vtV9QiMbV1c2' .
             '&domain-name=' . $request->domain() .
