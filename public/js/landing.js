c1bcc17fe72ea0072d464cd5251ed943297fe933
Franklin Decker III <frank@dzineer.com>
2020-04-16 17:04:46 +0700

    Working on Needs Analyzer Desktop

--- public/js/landing.js
+++ public/js/landing.js
@@ -3391,6 +3391,23 @@ __webpack_require__.r(__webpack_exports__);
       n = n.replace(/,/g, "");
       n = n.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       return "$" + n;
+    },
+    formatMoney: function formatMoney(amount) {
+      var decimalCount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
+      var decimal = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : ".";
+      var thousands = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : ",";
+      var symbol = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "$";
+
+      try {
+        decimalCount = Math.abs(decimalCount);
+        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+        var negativeSign = amount < 0 ? "-" : "";
+        var i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+        var j = i.length > 3 ? i.length % 3 : 0;
+        return symbol + negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+      } catch (e) {
+        console.log(e);
+      }
     }
   },
   methods: {
@@ -45295,7 +45312,7 @@ var render = function() {
           _c("amount-field", {
             attrs: {
               header: "Income Replacement Insurance Needed (Part I)",
-              value: _vm._f("formatAmount")(_vm.part1),
+              value: _vm._f("formatMoney")(_vm.part1),
               readonly: true
             }
           }),
@@ -45303,7 +45320,7 @@ var render = function() {
           _c("amount-field", {
             attrs: {
               header: "Total Additional Expenses (Part II)",
-              value: _vm._f("formatAmount")(_vm.part2),
+              value: _vm._f("formatMoney")(_vm.part2),
               readonly: true
             }
           }),
@@ -45311,7 +45328,7 @@ var render = function() {
           _c("amount-field", {
             attrs: {
               header: "Insurance Needed",
-              value: _vm._f("formatAmount")(_vm.total),
+              value: _vm._f("formatMoney")(_vm.total),
               readonly: true
             }
           }),
--- resources/assets/js/vuejs/components/NaTotal.vue
+++ resources/assets/js/vuejs/components/NaTotal.vue
@@ -7,12 +7,12 @@
                 <h2 class="tw-text-lg tw-text-white tw-font-semibold tw-uppercase tw-py-2" v-text="title"></h2>
             </div>
 
-            <div class="fields tw-py-4 tw-px-2">   
-                <amount-field header="Income Replacement Insurance Needed (Part I)" :value="part1 | formatAmount" :readonly="true"></amount-field>
-                <amount-field header="Total Additional Expenses (Part II)" :value="part2 | formatAmount" :readonly="true"></amount-field>
-                <amount-field header="Insurance Needed" :value="total | formatAmount" :readonly="true"></amount-field>
+            <div class="fields tw-py-4 tw-px-2">
+                <amount-field header="Income Replacement Insurance Needed (Part I)" :value="part1 | formatMoney" :readonly="true"></amount-field>
+                <amount-field header="Total Additional Expenses (Part II)" :value="part2 | formatMoney" :readonly="true"></amount-field>
+                <amount-field header="Insurance Needed" :value="total | formatMoney" :readonly="true"></amount-field>
                 <button class="tw-bg-primary tw-w-full hover:tw-bg-blue-700 tw-text-white tw-py-5 tw-px-8 tw-rounded focus:tw-outline-none focus:tw-shadow-outline" @click="onQuoteAmount">Quote Amount</button>
-            </div>            
+            </div>
 
         </div>
 
@@ -40,13 +40,28 @@ export default {
             n = n.replace(/,/g, "");
             n = n.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             return "$" + n;
+        },
+        formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",", symbol = "$") {
+            try {
+                decimalCount = Math.abs(decimalCount);
+                decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+                const negativeSign = amount < 0 ? "-" : "";
+
+                let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+                let j = (i.length > 3) ? i.length % 3 : 0;
+
+                return symbol + negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+            } catch (e) {
+                console.log(e)
+            }
         }
-    },    
+    },
     methods: {
         onQuoteAmount() {
             debugger;
             this.$emit('quoteFromCalculator');
-        }          
+        }
     }
 }
 </script>
