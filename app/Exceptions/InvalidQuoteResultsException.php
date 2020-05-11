d3280a8b992ced7873ecd043c18e0a3a42e77459
Franklin Decker III <frank@dzineer.com>
2020-05-11 15:10:25 +0700

    Added catch for termlifequote quoter.

---
+++ app/Exceptions/InvalidQuoteResultsException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Exceptions;
+
+
+class InvalidQuoteResultsException extends \Exception
+{
+
+}
--- app/Quoters/TermlifeQuoter.php
+++ app/Quoters/TermlifeQuoter.php
@@ -2,6 +2,9 @@
 
 namespace App\Quoters;
 
+use App\Exceptions\InvalidQuoteResultsException;
+use App\Exceptions\NoCarriersException;
+use App\Facades\AQLog;
 use App\Models\AccountQuote;
 use App\Models\AffiliateAd;
 use App\Models\Carrier;
@@ -70,116 +73,136 @@ class TermlifeQuoter {
 		return '/pd/'.sha1($company).'.pdf';
 	}
 
-	public function filterResults( $user, $results ) {
+	public function filterResults( $user, $results )
+    {
 
-		/* return filtered data */
-		$filteredPolicies = array();
+        /* return filtered data */
+        $filteredPolicies = array();
         $carrier = null;
-		$carriers = CategoriesInsurance::getCarriers($user->id,self::UNDERWRITTEN_CATEGORY);
+        $carriers = [];
 
-		// dd($carriers);
+        try {
+            $carriers = CategoriesInsurance::getCarriers($user->id, self::UNDERWRITTEN_CATEGORY);
 
-        $ad_record = AffiliateAd::where('affiliate_id', '=', $user->affiliate_id)
-            ->where('category_id', '=', self::UNDERWRITTEN_CATEGORY)
-            ->first();
+            // dd($carriers);
 
-        // dd($ad_record);
+            $ad_record = AffiliateAd::where('affiliate_id', '=', $user->affiliate_id)
+                ->where('category_id', '=', self::UNDERWRITTEN_CATEGORY)
+                ->first();
 
-        // echo json_encode($carriers); exit;
+            // dd($ad_record);
 
-        if ($ad_record && $ad_record->company_id !== 0) {
+            // echo json_encode($carriers); exit;
 
-            // echo json_encode($ad_record); exit;
+            if ($ad_record && $ad_record->company_id !== 0) {
 
-            $found = array_filter($carriers, function($carrier) use($ad_record) {
-                return $ad_record->company_id === $carrier->company_id;
-            });
+                // echo json_encode($ad_record); exit;
 
-            // echo json_encode($found); exit;
+                $found = array_filter($carriers, function ($carrier) use ($ad_record) {
+                    return $ad_record->company_id === $carrier->company_id;
+                });
 
-            if (! $found) {
-                $carrier = Carrier::find($ad_record->company_id);
-                dd($carrier);
+                // echo json_encode($found); exit;
 
-                $carriers[] = $carrier;
+                if (!$found) {
+                    $carrier = Carrier::find($ad_record->company_id);
+                    dd($carrier);
+
+                    $carriers[] = $carrier;
+                }
+
+            }
+
+            // dd($ad_record);
+
+            // echo json_encode($carriers); exit;
+
+            $carrierIds = array();
+            $carriersLookup = array();
+
+            // echo '<div><pre><br>results: <br>' . print_r( $results, true ) . '<br></pre></div>';
+            // echo '<div><pre><br>results: <br>' . print_r( $carriers, true ) . '<br></pre></div>'; exit;
+
+            // echo json_encode($carriers); exit;
+
+            if (!is_array($carriers) || !count($carriers)) {
+                throw new NoCarriersException("No carriers available");
+            }
+
+            foreach ($carriers as $carrier) {
+                // echo '<div><pre><br>results: <br>' . print_r( $carrier, true ) . '<br></pre></div>'; exit;
+                if ($carrier->selected != 0 || ($ad_record && $ad_record->company_id === $carrier->company_id)) {
+                    $carrierIds[] = $carrier->company_id;
+                }
+                $carriersLookup[$carrier->company_id] = $carrier;
+
+            }
+
+            // echo '<div><pre><br>results: <br>' . print_r( $carriersLookup, true ) . '<br></pre></div>'; exit;
+            // echo '<div><pre><br>results sss: <br>' . print_r( $results, true ) . '<br></pre></div>'; exit;
+            // exit;
+
+            // dd($results);
+
+            AQLog::quote('TermlifeQuoter results: ' . print_r($results, true));
+
+            if (is_array($results) && count($results)) {
+                foreach ($results as $key => $result) {
+                    if ($key !== 'inputs') {
+                        //echo '<div><pre><br>key: <br>' . print_r( $key, true ) . '<br></pre></div>';
+                        // echo '<div><pre><br>result: <br>' . print_r( $result, true ) . '<br></pre></div>'; exit;
+                        // $carrier_fk = $result['CompanyFK'];
+                        // echo '<div><pre><br>result: <br>' . print_r($result, true) . '<br></pre></div>';
+                        if (in_array(intval($result['CompanyFK']), $carrierIds)) {
+                            $carrierDetails = [];
+
+                            // echo "in array"; exit;
+                            $carrier = $carriersLookup[intval($result['CompanyFK'])];
+                            // echo '<div><pre><br>carrier: <br>' . print_r($carrier, true) . '<br></pre></div>';
+                            $result['underwriting_guidelines_link'] = $this->genUnderwritingGuidelinesLink($result['CompanyName']);
+                            $result['policy_details_link'] = $this->genPolicyDetailsLink($result['CompanyName']);
+                            $result['link1'] = $result['underwriting_guidelines_link'];
+                            $result['link2'] = $result['policy_details_link'];
+
+                            $carrierDetails['name'] = $result['CompanyName'];
+                            $carrierDetails['address1'] = $result['Address1'];
+                            $carrierDetails['address2'] = $result['Address2'];
+                            $carrierDetails['addressHeader'] = $result['AddressHeader'];
+                            $carrierDetails['addressTrailer'] = $result['AddressTrailer'];
+                            $carrierDetails['assets'] = $result['Assets'];
+                            $carrierDetails['city'] = $result['City'];
+                            $carrierDetails['disclaimer'] = $result['Disclaimer'];
+                            $carrierDetails['insuranceInForce'] = $result['InsuranceInForce'];
+                            $carrierDetails['lastChangedOn'] = $result['LastChangedOn'];
+                            $carrierDetails['liabilities'] = $result['Liabilities'];
+                            $carrierDetails['nwYorkCompany'] = $result['NewYorkCompany'];
+                            $carrierDetails['phone'] = $result['PhoneNumber'];
+                            $carrierDetails['reviewedOn'] = $result['ReviewedOn'];
+                            $carrierDetails['stateAbbreviation'] = $result['StateAbbreviation'];
+                            $carrierDetails['website'] = $result['Website'];
+                            $carrierDetails['zipCode'] = $result['ZipCode'];
+                            $carrierDetails['reference'] = $result['Reference'];
+
+                            $result['CarrierDetails'] = $carrierDetails;
+
+                            $filteredPolicies[] = $result;
+
+                        }
+                    }
+                }
+            } else {
+                throw new InvalidQuoteResultsException("Invalid quote results");
             }
 
         }
+		catch(InvalidQuoteResultsException $e) {
+            AQLog::quote('Invalid TermlifeQuoter results: ' .  print_r($results,true));
+        }
+        catch (NoCarriersException $e) {
+            AQLog::quote('Invalid TermlifeQuoter no carriers: ' .  print_r($carriers,true));
+        }
 
-        // dd($ad_record);
-
-        // echo json_encode($carriers); exit;
-
-        $carrierIds = array();
-		$carriersLookup = array();
-
-		// echo '<div><pre><br>results: <br>' . print_r( $results, true ) . '<br></pre></div>';
-		// echo '<div><pre><br>results: <br>' . print_r( $carriers, true ) . '<br></pre></div>'; exit;
-
-        // echo json_encode($carriers); exit;
-
-		foreach( $carriers as $carrier ) {
-			// echo '<div><pre><br>results: <br>' . print_r( $carrier, true ) . '<br></pre></div>'; exit;
-			if ( $carrier->selected != 0 || ($ad_record && $ad_record->company_id === $carrier->company_id )) {
-				$carrierIds[] = $carrier->company_id;
-			}
-			$carriersLookup[ $carrier->company_id ] = $carrier;
-
-		}
-
-		// echo '<div><pre><br>results: <br>' . print_r( $carriersLookup, true ) . '<br></pre></div>'; exit;
-		// echo '<div><pre><br>results sss: <br>' . print_r( $results, true ) . '<br></pre></div>'; exit;
-		// exit;
-
-        // dd($results);
-
-		foreach( $results as $key => $result )  {
-			if ( $key !== 'inputs' )
-			{
-				//echo '<div><pre><br>key: <br>' . print_r( $key, true ) . '<br></pre></div>';
-				// echo '<div><pre><br>result: <br>' . print_r( $result, true ) . '<br></pre></div>'; exit;
-				// $carrier_fk = $result['CompanyFK'];
-				// echo '<div><pre><br>result: <br>' . print_r($result, true) . '<br></pre></div>';
-				if (in_array(intval($result['CompanyFK']), $carrierIds))
-				{
-					$carrierDetails = [];
-
-					// echo "in array"; exit;
-					$carrier = $carriersLookup[intval($result['CompanyFK'])];
-					// echo '<div><pre><br>carrier: <br>' . print_r($carrier, true) . '<br></pre></div>';
-					$result['underwriting_guidelines_link'] = $this->genUnderwritingGuidelinesLink($result['CompanyName']);
-					$result['policy_details_link']          = $this->genPolicyDetailsLink($result['CompanyName']);
-					$result['link1']                        = $result['underwriting_guidelines_link'];
-					$result['link2']                        = $result['policy_details_link'];
-
-					$carrierDetails['name']                 = $result['CompanyName'];
-					$carrierDetails['address1']             = $result['Address1'];
-					$carrierDetails['address2']             = $result['Address2'];
-					$carrierDetails['addressHeader']        = $result['AddressHeader'];
-					$carrierDetails['addressTrailer']       = $result['AddressTrailer'];
-					$carrierDetails['assets']               = $result['Assets'];
-					$carrierDetails['city']        			= $result['City'];
-					$carrierDetails['disclaimer']        	= $result['Disclaimer'];
-					$carrierDetails['insuranceInForce']     = $result['InsuranceInForce'];
-					$carrierDetails['lastChangedOn']        = $result['LastChangedOn'];
-					$carrierDetails['liabilities']        	= $result['Liabilities'];
-					$carrierDetails['nwYorkCompany']        = $result['NewYorkCompany'];
-					$carrierDetails['phone']        		= $result['PhoneNumber'];
-					$carrierDetails['reviewedOn']       	= $result['ReviewedOn'];
-					$carrierDetails['stateAbbreviation']    = $result['StateAbbreviation'];
-					$carrierDetails['website']        		= $result['Website'];
-					$carrierDetails['zipCode']       	    = $result['ZipCode'];
-					$carrierDetails['reference']            = $result['Reference'];
-
-					$result['CarrierDetails']               = $carrierDetails;
-
-					$filteredPolicies[]                     = $result;
-
-				}
-			}
-		}
-
-		// echo '<div><pre><br>carriersLookup: <br>' . print_r( $carriersLookup, true ) . '<br></pre></div>';
+        // echo '<div><pre><br>carriersLookup: <br>' . print_r( $carriersLookup, true ) . '<br></pre></div>';
 		// echo '<div><pre><br>filteredPolicies: <br>' . print_r( $filteredPolicies, true ) . '<br></pre></div>'; exit;
 
 		return $filteredPolicies;
@@ -218,7 +241,15 @@ class TermlifeQuoter {
             echo '<pre><br>mappedFields: <br>' . print_r( $mappedFields, true );
         }
 
-        $quote = $gw->getQuote( $mappedFields );
+        $quote = null;
+
+        try {
+            $quote = $gw->getQuote( $mappedFields );
+            AQLog::quote('TermlifeQuoter gw->getQuote result: ' .  print_r($quote,true));
+        }
+        catch (InvalidQuoteResultsException $e) {
+            AQLog::quote('Invalid TermlifeQuoter results: ' .  print_r($quote,true));
+        }
 
 		// echo "<br>quote: " . print_r($quote,true);
 
