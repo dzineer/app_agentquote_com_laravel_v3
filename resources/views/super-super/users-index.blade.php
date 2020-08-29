84b203d8aaf93351a4d85495ec065f2a4246c1b5
Franklin Decker III <frank@dzineer.com>
2020-08-29 23:20:50 +0700

    Added Super Super Users select login as form

--- resources/views/super-super/users-index.blade.php
+++ resources/views/super-super/users-index.blade.php
@@ -30,7 +30,7 @@
                                 <label for="exampleInputEmail1">User</label>
                                 <select name="user">
                                     @foreach($users as $user)
-                                        <option value="{{ $user->id }}">{{ $user->name - $user->email }}</option>
+                                        <option value="{{ $user['id'] }}">{{ $user['name'] - $user['email'] }}</option>
                                     @endforeach
                                 </select>
                                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
